c88eb86d1b08db4fee5024f6fd60a5226056899f
Daviduo <davide0400@gmail.com>
2023-01-27 16:28:00 +0100

    added examples

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ class Salary {
     private $thirteenthMonthly;
     private $fourteenthMonthly;
 
+    public function __construct($monthly, $thirteenthMonthly, $fourteenthMonthly) {
+
+        $this->setMonthly($monthly);
+        $this->setThirteenthMonthly($thirteenthMonthly);
+        $this->setFourteenthMonthly($fourteenthMonthly);
+
+    }
+
     public function getMonthly() {
 
         return $this -> monthly;
@@ -34,44 +42,43 @@ class Salary {
         $this -> fourteenthMonthly = $fourteenthMonthly;
     }
 
-    public function getAnnualSalaryHtml(){
+    public function getAnnualSalary() {
 
-        $AnnualSalary = $this->monthly * 12
-            + ($this->thirteenthMonthly ? 1 : 0)
-            + ($this->fourteenthMonthly ? 1 : 0);
+        $monthly = $this->getMonthly();
+        return $monthly * 12
+            + ($this->getThirteenthMonthly() ? $monthly : 0 ) 
+            + ($this->getFourteenthMonthly() ? $monthly : 0 );
+        
+    }
+
+    public function getHtml(){
+
+        $AnnualSalary =
+            "Salario Mensile: " . $this->getMonthly() . "<br>"
+            . "Tredicesima: " . ($this->thirteenthMonthly ? "✅" : "❌" ) . "<br>"
+            . "Quattordicesima: " . ($this->fourteenthMonthly ? "✅" : "❌" ) . "<br>"
+            . "Salario Annuale: " . $this->getAnnualSalary() . "<br>";
         return $AnnualSalary;
             
     }
-
 
 }
 
 class Person {
 
     //Variabili
-    private $id;
     private $name;
     private $surname;
     private $dateOfBirth;
     private $fiscalCode;
 
-    public function __construct($id, $name, $surname, $dateOfBirth, $fiscalCode) {
+    public function __construct( $name, $surname, $dateOfBirth, $fiscalCode) {
 
-        $this -> setId($id);
         $this -> setName($name);
         $this -> setSurname($surname);
         $this -> setDateOfBirth($dateOfBirth);
         $this -> setFiscalCode($fiscalCode);
 
-    }
-
-    public function getId() {
-
-        return $this -> id;
-    }
-    public function setId($id) {
-
-        $this -> id = $id;
     }
 
     public function getName() {
@@ -112,23 +119,24 @@ class Person {
     public function getHtml() {
 
         return 
-               $this -> getId() . "<br>"
-             . $this -> getName() . "<br>"
+
+               $this -> getName() . "<br>"
              . $this -> getSurname() . "<br>"
-             . $this -> getDateOfBirth() . "<br>"
-             . $this -> getFiscalCode() . "<br>";
+             . "Data di nascita: " . $this -> getDateOfBirth() . "<br>"
+             . "Codice fiscale: " . $this -> getFiscalCode() . "<br>";
+
     }
 }
 
 class Employee extends Person {
 
     //Variabili
-    private array $salary;
+    private Salary $salary;
     private $hireDate;
 
-    public function __construct($id, $name, $surname, $dateOfBirth, $fiscalCode, array $salary, $hireDate) {
+    public function __construct($name, $surname, $dateOfBirth, $fiscalCode, Salary $salary, $hireDate) {
 
-        parent::__construct($id, $name, $surname, $dateOfBirth, $fiscalCode);
+        parent::__construct($name, $surname, $dateOfBirth, $fiscalCode);
         $this -> setSalary($salary);
         $this -> setHireDate($hireDate);
 
@@ -148,7 +156,7 @@ class Employee extends Person {
 
     public function getHireDate() {
 
-        return $this -> salary;
+        return $this -> hireDate;
 
     }
     public function setHireDate($hireDate) {
@@ -157,12 +165,19 @@ class Employee extends Person {
 
     }
 
+    public function getAnnualSalary() {
+
+        return $this->getSalary()->getAnnualSalary();
+
+    }
+
     public function getHtml() {
 
         return 
-               parent :: getHtml() . "<br>"
-             . $this -> getSalary() . "<br>"
-             . $this -> getHireDate() . "<br>";
+               parent :: getHtml()
+            . "Data di assunzione: " . $this -> getHireDate() . "<br>"
+             . $this -> getSalary() -> getHtml();
+           
     }
 }
 
@@ -173,9 +188,9 @@ class Boss extends Person {
     private $dividend;
     private $bonus;
 
-    public function __construct($id, $name, $surname, $dateOfBirth, $fiscalCode, $dividend, $bonus) {
+    public function __construct($name, $surname, $dateOfBirth, $fiscalCode, $dividend, $bonus) {
 
-        parent::__construct($id, $name, $surname, $dateOfBirth, $fiscalCode);
+        parent::__construct($name, $surname, $dateOfBirth, $fiscalCode);
         $this -> setDividend($dividend);
         $this -> setBonus($bonus);
 
@@ -204,19 +219,39 @@ class Boss extends Person {
 
     }
 
-    public function getAnnualSalaryHtml(){
+    public function getAnnualSalary(){
 
-        $AnnualSalary = $this->dividend * 12
-            + $this->bonus;
-        return $AnnualSalary;
+        return $this->getDividend() * 12
+            + $this->getBonus();
             
     }
 
     public function getHtml() {
 
-        return 
-               parent :: getHtml() . "<br>"
-             . $this -> getDividend() . "<br>"
-             . $this -> getBonus() . "<br>";
+        return
+            parent::getHtml()
+            . "Dividendo: " . $this->getDividend() . "<br>"
+            . "Bonus: " . $this->getBonus() . "<br>"
+            . "Salario Annuale: " . $this->getAnnualSalary();
+
     }
 }
+
+$salary1 = new Salary(5837, true, true);
+echo $salary1->getHtml();
+
+echo "___________________ <br>";
+
+$employee1 = new Employee("Giorgia", "Meloni", "1977-01-15", "MLNGRG77A55H501C", $salary1, "1997-03-21");
+echo $employee1 -> getHtml();
+
+echo "___________________ <br>";
+
+$boss1 = new Boss("Gianna", "Nannini", "195-06-14", "NNNGNN54H54I726A", 7675, 15000);
+echo $boss1->getHtml();
+
+echo " <br><br> esempio di Person: </br>";
+
+$person1 = new Person("Mario", "Rossi", "25-01-1995", "MRARSS95A25H501P");
+echo $person1->getHtml();
+
